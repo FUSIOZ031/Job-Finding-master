4f41d4cc5b08f46dda798de17213cf1372523465
Minh <hoaminhgranite2@gmail.com>
2022-06-09 15:41:58 +0700

    e

--- user/index.php
+++ user/index.php
@@ -53,8 +53,8 @@ require_once("../db.php");
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>J</b>P</span>
       <!-- logo for regular state and mobile devices -->
-
-      <span class="fa fa-globe"> <b>BK</b> <b>Job</b> <b>Portal</b></span>
+      <img src=img/logo.png width="50" height="45" alt="">
+      <span> <b>BK</b> <b>Job</b> <b>Portal</b></span>
     </a>
 
     <!-- Header Navbar: style can be found in header.less -->
