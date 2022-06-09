7073d73ce585207b1f8a0d14d10d06cb4f02f184
FUSIOZ031 <62032230+FUSIOZ031@users.noreply.github.com>
2022-06-09 13:27:49 +0700

    Edit login font

--- login-candidates.php
+++ login-candidates.php
@@ -25,13 +25,6 @@ if(isset($_SESSION['id_user']) || isset($_SESSION['id_company'])) {
   <!-- iCheck -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/blue.css">
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
